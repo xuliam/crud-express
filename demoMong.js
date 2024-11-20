a2bebf9e040d1ffb5a612cafff58bfeac4c65e0a
xu lian <xulian@xudeMacBook-Air.local>
2024-11-20 09:58:53 +0800

    add

--- demoMong.js
+++ demoMong.js
@@ -37,4 +37,18 @@ const userSchema = new Schema({
 
   admin.save()
 
+
+//   async function run() {
+//     // 4. Connect to MongoDB
+//     await connect('mongodb://127.0.0.1:27017/test');
+  
+//     const user = new User({
+//       name: 'Bill',
+//       email: 'bill@initech.com',
+//       avatar: 'https://i.imgur.com/dM7Thhn.png'
+//     });
+//     await user.save();
+  
+//     console.log(user.email); // 'bill@initech.com'
+//   }
   
